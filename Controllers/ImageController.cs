d72f4a7f6cca59fe915bb10068ee542d220ba274
Riley Andert <Riley.Andert@alaskaair.com>
2018-03-19 16:46:03 -0700

    adding files

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+
+namespace TestApi.Controllers
+{
+   
+    public class ImageController : ApiController
+    {
+        /*        [HttpPost]
+                [Route("api/image")]
+                public void StoreImage([FromBody] string Image)
+                {
+
+
+                    var hit = "hit";
+                }
+
+
+                [HttpPost]
+                [Route("api/image")]
+                public void StoreImage([FromBody] HttpContent Image)
+                {
+
+
+                    var hit = "hit";
+                }
+
+                [HttpPost]
+                [Route("api/image")]
+                public void StoreImage([FromBody] StringContent Image)
+                {
+
+
+                    var hit = "hit";
+                }*/
+
+        [HttpPost]
+        [Route("api/image")]
+        public void StoreImage([FromBody] string image)
+        {
+
+
+            var hit = "hit";
+        }
+
+
+
+
+    }
+}
+
